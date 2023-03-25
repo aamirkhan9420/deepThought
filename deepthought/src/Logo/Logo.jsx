c9a66060eff33699fb088bfef7fcd143941b0bcf
aamirkhan9420 <aamirkhan.z9373@gmail.com>
2023-03-25 18:04:44 +0530

    app initialized and navbar left part almost done

---
+++ deepthought/src/Logo/Logo.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import  "./Logo.css"
+function Logo() {
+  return (
+    
+    <div className='nav_left_container'>
+  
+     <div className='logo_round'> </div>
+     <div className='name_box'>
+        <h2 className='deep'>DEEP</h2>
+       <h2 className='thought'>THOUGHT</h2>
+    </div>
+      
+
+      
+
+     
+    </div>
+  )
+}
+
+export default Logo
---
+++ deepthought/src/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Logo from '../Logo/Logo'
+import "./Navbar.css"
+function Navbar() {
+  return (
+    <div className='parent_nav_container'>
+   
+        <Logo />
+     
+    </div>
+  )
+}
+
+export default Navbar
